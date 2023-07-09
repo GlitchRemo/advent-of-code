6490b550b104c1b4d0c1ba6639338f3df1b7461c
Riya <GlitchRemo@users.noreply.github.com>
2023-07-09 14:07:08 +0530

    feat:add part1 solution to calculate unique houses visited

--- 2015/day3-count-houses/main.js
+++ 2015/day3-count-houses/main.js
@@ -1,7 +1,8 @@
+const { readFileSync } = require("fs");
 const { countHousesVisited } = require("./src/count-houses");
 
 const main = () => {
-  const directions = "<^^>";
+  const directions = readFileSync("./resources/directions.txt", "utf-8");
   console.log(countHousesVisited(directions));
 };
 
