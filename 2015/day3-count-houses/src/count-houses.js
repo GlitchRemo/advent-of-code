004845e4be65e3bad9179c05f5d7a87660868ed4
Riya <GlitchRemo@users.noreply.github.com>
2023-07-09 13:13:55 +0530

    Add: Implements helper function to provide the next Coordinate in East

--- 2015/day3-count-houses/src/count-houses.js
+++ 2015/day3-count-houses/src/count-houses.js
@@ -1,2 +1,15 @@
-const countHouses = (directions) => directions.length + 1;
-exports.countHouses = countHouses;
+const nextCoordinateInEast = ({ x, y }, direction) => ({ x: x + 1, y: y + 0 });
+
+const countHouses = (directions) => {
+  let currentCoordinate = { x: 0, y: 0 };
+  const housesVisited = new Set([JSON.stringify(currentCoordinate)]);
+
+  [...directions].forEach((direction) => {
+    currentCoordinate = nextCoordinateInEast(currentCoordinate, direction);
+    housesVisited.add(JSON.stringify(currentCoordinate));
+  });
+
+  return housesVisited.size;
+};
+
+module.exports = { countHouses, nextCoordinateInEast };
--- 2015/day3-count-houses/test/count-houses-test.js
+++ 2015/day3-count-houses/test/count-houses-test.js
@@ -1,6 +1,6 @@
 const assert = require("assert");
 const { describe, it } = require("node:test");
-const { countHouses } = require("../src/count-houses");
+const { countHouses, nextCoordinateInEast } = require("../src/count-houses");
 
 describe("countHouses", () => {
   it("should be 1 if Santa doesn't move from starting location", () => {
@@ -9,5 +9,17 @@ describe("countHouses", () => {
 
   it("should be 2 if Santa moves only once", () => {
     assert.strictEqual(countHouses(">"), 2);
+  });
+});
+
+describe("nextCoordinateInEast", () => {
+  it("should shift the coodinate in east", () => {
+    const coordinate = { x: 0, y: 0 };
+    const nextCoordinate = { x: 1, y: 0 };
+
+    assert.deepStrictEqual(
+      nextCoordinateInEast(coordinate, ">"),
+      nextCoordinate
+    );
   });
 });
