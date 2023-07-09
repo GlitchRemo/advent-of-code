fa3a0f468cdd88b571d19d61c5f1b3c317d486c8
Riya <GlitchRemo@users.noreply.github.com>
2023-07-09 13:47:45 +0530

    Feat: Add multi-direction movement of Santa

--- 2015/day3-count-houses/test/count-houses-test.js
+++ 2015/day3-count-houses/test/count-houses-test.js
@@ -10,11 +10,19 @@ describe("countHouses", () => {
   it("should be 2 if Santa moves only once", () => {
     assert.strictEqual(countHouses(">"), 2);
   });
+
+  it("should be the count of unique houses visited by Santa if Santa moves in multiple directions", () => {
+    assert.strictEqual(countHouses("^>v<"), 4);
+    assert.strictEqual(countHouses("^v^v^v^v^v"), 2);
+  });
 });
 
 describe("nextCoordinate", () => {
   it("should give the immediate next coordinate in East", () => {
-    assert.deepStrictEqual(nextCoordinate({ x: 0, y: 0 }, ">"), { x: 1, y: 0 });
+    assert.deepStrictEqual(nextCoordinate({ x: 0, y: 0 }, ">"), {
+      x: 1,
+      y: 0,
+    });
   });
 
   it("should give the immediate next coordinate in West", () => {
