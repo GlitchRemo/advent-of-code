9b891ff79aa0124614cac92f71e58faf1e7f65df
Riya <GlitchRemo@users.noreply.github.com>
2023-07-10 10:47:13 +0530

    refactor:seperated the parts of day3 challange from `main`

--- 2015/day3-house-visit/main.js
+++ 2015/day3-house-visit/main.js
@@ -1,17 +1,20 @@
 const fs = require("fs");
 const { countHousesVisited, countHousesVisitedByTwoSantas } = require("./src/count-houses");
 
+const part1 = (directions) => {
+  const housesVisitedByOneSanta = countHousesVisited(directions);
+  console.log(`One Santa delivers present to ${housesVisitedByOneSanta} houses at least once`);
+};
+
+const part2 = (directions) => {
+  const housesVisitedByTwoSantas = countHousesVisitedByTwoSantas(directions);
+  console.log(`Two Santas deliver present to ${housesVisitedByTwoSantas} houses at least once`);
+};
+
 const main = () => {
   const directions = fs.readFileSync("./resources/directions.txt", "utf-8");
-  const countOfHousesVisitedByOneSanta = countHousesVisited(directions);
-  const countOfHousesVisitedByTwoSantas = countHousesVisitedByTwoSantas(directions);
-
-  console.log(
-    `One Santa delivers present to ${countOfHousesVisitedByOneSanta} houses at least once`
-  );
-  console.log(
-    `Two Santas deliver present to ${countOfHousesVisitedByTwoSantas} houses at least once`
-  );
+  part1(directions);
+  part2(directions);
 };
 
 main();
